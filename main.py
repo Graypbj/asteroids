18d57b09f5fb10feb6c2ef6fef9b17e76973d95d
Graypbj <graypbj@gmail.com>
2024-10-30 20:05:20 -0600

    Added framerate limiter and circle sprite

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ import pygame
 from constants import *
 
 def main():
-    pygame.init
+    pygame.init()
     print("Starting asteroids!")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -14,9 +17,7 @@ def main():
         color = [0, 0, 0]
         screen.fill(color)
         pygame.display.flip()
-
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
