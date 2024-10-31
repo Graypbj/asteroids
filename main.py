29280aa74a20780da6f2b6de34f73e9f6d0dcd38
Graypbj <graypbj@gmail.com>
2024-10-30 19:50:06 -0600

    Added game window and closing logic

--- main.py
+++ main.py
@@ -2,7 +2,19 @@ import pygame
 from constants import *
 
 def main():
+    pygame.init
     print("Starting asteroids!")
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        color = [0, 0, 0]
+        screen.fill(color)
+        pygame.display.flip()
+
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
