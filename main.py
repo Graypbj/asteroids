985107b3acd78b57b796d3a0e5d6080e56c80c08
Graypbj <graypbj@gmail.com>
2024-10-31 10:08:10 -0600

    Created containers and groups to hold sprites

--- main.py
+++ main.py
@@ -3,20 +3,37 @@ from constants import *
 from player import Player
 
 def main():
+    # Initialize game and add constants for screen size and clock speed
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    # Create containers and groups for sprites
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
+    # Create player instance and add delta for the player
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
+    # Game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
+
+        # Draw black screen
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)    
+
+        # Iterate over groups and update their position and draw them
+        for object in updatable:
+            object.update(dt)
+        
+        for object in drawable:
+            object.draw(screen)
+        
+        # Display the screen
         pygame.display.flip()
 
         # Limit framerate
